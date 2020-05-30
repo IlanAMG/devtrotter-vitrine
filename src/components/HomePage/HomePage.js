3fd57248cc477876bce299c3bb7ae3c4a1b72840
Ilan Amzallag <59542957+IlanAMG@users.noreply.github.com>
2020-05-30 03:10:24 +0200

    v0.4.7

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -28,27 +28,33 @@ export const HomePage = () => {
             <div className='container-constel'>
                 <svg width="90" height="400" viewBox="0 0 13 309" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <circle onClick={() => scrollToRef(accueil)} cx="6.5" cy="6.5" r="6.5" fill="url(#paint0_radial)">
-                        <animateMotion id='1anim1' dur="2s" begin='0s; 1anim2.end' path="M-20,0 20,0" fill="freeze" />
-                        <animateMotion id='1anim2' dur="2s" begin='1anim1.end' path="M20,0 -20,0" fill="freeze" />
+                        <animateMotion id='1anim1' dur="2s" begin='0s; 1anim2.end' path="M-15,0 20,0" fill="freeze" />
+                        <animateMotion id='1anim2' dur="2.3s" begin='1anim1.end' path="M20,0 -15,0" fill="freeze" />
                     </circle>
                     <circle onClick={() => scrollToRef(quisommesnous)} cx="6.5" cy="53.5" r="6.5" fill="url(#paint1_radial)">
-                        <animateMotion id='2anim1' dur="2s" begin='0s; 2anim2.end' path="M20,0 -20,0" fill="freeze" />
+                        <animateMotion id='2anim1' dur="2.3s" begin='0.76s; 2anim2.end' path="M20,0 -20,0" fill="freeze" />
                         <animateMotion id='2anim2' dur="2s" begin='2anim1.end' path="M-20,0 20,0" fill="freeze" />
                     </circle>
                     <circle onClick={() => scrollToRef(realisations)} cx="6.5" cy="104.5" r="6.5" fill="url(#paint2_radial)">
-                        <animateMotion id='3anim1' dur="2s" begin='0s; 3anim2.end' path="M-20,0 20,0" fill="freeze" />
+                        <animateMotion id='3anim1' dur="2s" begin='0.98s; 3anim2.end' path="M-20,0 20,0" fill="freeze" />
                         <animateMotion id='3anim2' dur="2s" begin='3anim1.end' path="M20,0 -20,0" fill="freeze" />
                     </circle>
                     <circle onClick={() => scrollToRef(nosoffres)} cx="6.5" cy="155.5" r="6.5" fill="url(#paint3_radial)">
-                        <animateMotion id='4anim1' dur="2s" begin='0s; 4anim2.end' path="M20,0 -20,0" fill="freeze" />
-                        <animateMotion id='4anim2' dur="2s" begin='4anim1.end' path="M-20,0 20,0" fill="freeze" />
+                        <animateMotion id='4anim1' dur="2.3s" begin='0.36s; 4anim2.end' path="M15,0 -20,0" fill="freeze" />
+                        <animateMotion id='4anim2' dur="2s" begin='4anim1.end' path="M-20,0 15,0" fill="freeze" />
                     </circle>
                     <circle cx="6.5" cy="204.5" r="6.5" fill="url(#paint4_radial)">
-                        <animateMotion id='5anim1' dur="2s" begin='0s; 5anim2.end' path="M-20,0 20,0" fill="freeze" />
+                        <animateMotion id='5anim1' dur="2s" begin='0.24s; 5anim2.end' path="M-20,0 20,0" fill="freeze" />
                         <animateMotion id='5anim2' dur="2s" begin='5anim1.end' path="M20,0 -20,0" fill="freeze" />
                     </circle>
-                    <circle cx="6.5" cy="253.5" r="6.5" fill="url(#paint5_radial)" />
-                    <circle cx="6.5" cy="302.5" r="6.5" fill="url(#paint6_radial)" />
+                    <circle cx="6.5" cy="253.5" r="6.5" fill="url(#paint5_radial)">
+                        <animateMotion id='6anim1' dur="2s" begin='0.2s; 6anim2.end' path="M25,0 -20,0" fill="freeze" />
+                        <animateMotion id='6anim2' dur="2.3s" begin='6anim1.end' path="M-20,0 25,0" fill="freeze" />
+                    </circle>
+                    <circle cx="6.5" cy="302.5" r="6.5" fill="url(#paint6_radial)">
+                        <animateMotion id='7anim1' dur="2.3s" begin='0.3s; 7anim2.end' path="M-15,0 25,0" fill="freeze" />
+                        <animateMotion id='7anim2' dur="2.3s" begin='7anim1.end' path="M25,0 -15,0" fill="freeze" />
+                    </circle>
                     <line x1="6.47291" y1="253" x2="6.4729" y2="303" stroke="#F6F2F2" stroke-width="0.945822" />
                     <line x1="6.47291" y1="203" x2="6.4729" y2="253" stroke="#F6F2F2" stroke-width="0.945822" />
                     <line x1="6.47291" y1="156" x2="6.4729" y2="206" stroke="#F6F2F2" stroke-width="0.945822" />
